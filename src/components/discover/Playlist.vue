b4524b1df7cd79f435a69eb485d9eb510580e2d4
DeFeNG <1437931235@qq.com>
2017-10-28 20:21:57 +0800

    完成歌单页面的分类

--- src/components/discover/Playlist.vue
+++ src/components/discover/Playlist.vue
@@ -6,9 +6,21 @@
 			<div class="panel-title">
 				<div class="p-t-text">{{cat}}</div>
 				<div class="cat">
-					<a href="javascript:;">选择分类<em></em></a>
-					<div class="cat-panel">
-						
+					<a href="javascript:;" @click.stop.prevent="toggleCatPanel">选择分类 <em></em></a>
+					<div class="cat-panel" v-show="catPanelShow == true">
+						<h3><a href="javascript:;" @click.stop.prevent="changeCat('全部')">全部风格</a></h3>
+						<div class="c-p-row" v-for="(cat, k) in allCat">
+							<div class="c-p-title">
+								<i class="c-p-icon" :class="k"></i>
+								{{ cat.name }}
+							</div>
+							<div class="c-p-content">
+								<div v-for="c in cat.cats" class="c-p-c-item">
+									<a href="javascript:;" @click.stop.prevent="changeCat(c)">{{ c }}</a>
+									<span>|</span>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div class="hot-new">
@@ -68,16 +80,33 @@
 				cat: '全部',
 				allCat: {
 					// 语种
-					lan: ["华语", "欧美", "日语","韩语","粤语","小语种"],
+					lan: {
+						name: '语种',
+						cats: ["华语", "欧美", "日语","韩语","粤语","小语种"]
+					},
 					// 风格
-					style: ['流行','摇滚','民谣','电子','舞曲','说唱','轻音乐','爵士','乡村','R&B/Soul','古典','民族','英伦','金属','朋克','蓝调','雷鬼','世界音乐','拉丁','另类/独立','New Age','古风','后摇','Bossa Nova'],
+					style: {
+						name: '风格',
+						cats: ['流行','摇滚','民谣','电子','舞曲','说唱','轻音乐','爵士','乡村','R&B/Soul','古典','民族','英伦','金属','朋克','蓝调','雷鬼','世界音乐','拉丁','另类/独立','New Age','古风','后摇','Bossa Nova']
+					},
 					// 场景
-					occasion: ['清晨','夜晚','学习','工作','午休','下午茶','地铁','驾车','运动','旅行','散步','酒吧'],
+					occasion: {
+						name: '场景',
+						cats: ['清晨','夜晚','学习','工作','午休','下午茶','地铁','驾车','运动','旅行','散步','酒吧']
+					},
 					// 情感
-					emotion: ['怀旧','清新','浪漫','性感','伤感','治愈','放松','孤独','感动','兴奋','快乐','安静','思念'],
+					emotion: {
+						name: '情感',
+						cats: ['怀旧','清新','浪漫','性感','伤感','治愈','放松','孤独','感动','兴奋','快乐','安静','思念']
+					},
 					// 主题
-					theme: ['影视原声','ACG','校园','游戏','70后','80后','90后','网络歌曲','KTV','经典','翻唱','吉他','钢琴','器乐','儿童','榜单','00后']
-				}
+					theme: {
+						name: '主题',
+						cats: ['影视原声','ACG','校园','游戏','70后','80后','90后','网络歌曲','KTV','经典','翻唱','吉他','钢琴','器乐','儿童','榜单','00后']
+					}
+				},
+				// 是否打开分类面板
+				catPanelShow: false
 			}
 		},
 		computed: {
@@ -127,9 +156,18 @@
 					this.$store.dispatch('showTip', '请更换村通网')
 				})
 			},
+			// 切换分类
+			changeCat(c) {
+				this.cat = c
+				this.catPanelShow = false
+			},
 			// 热门、最新切换
 			toggleOrder(o) {
 				this.order = o
+			},
+			// 打开、关闭分类面板
+			toggleCatPanel() {
+				this.catPanelShow = !this.catPanelShow
 			}
 		},
 		created(){
@@ -138,6 +176,11 @@
 		watch: {
 			// 页数变化了
 			CurrentPage () {
+				this.getPlayLists()
+				window.scrollTo(0, 0)
+			},
+			// 切换了分类
+			cat(n, o) {
 				this.getPlayLists()
 			},
 			// 切换了热门、最新
@@ -167,7 +210,7 @@
 	/*版面标题*/
 	.panel-title {
 		width: 100%;
-		height: 35px;
+		height: 40px;
 		border-bottom: 2px solid rgb(193,13,12);
 		display: flex;
 		justify-content: flex-start;
@@ -178,6 +221,7 @@
 	}
 	/*分类*/
 	.cat {
+		position: relative;
 		border: 1px solid #ccc;
 		border-radius: 4px;
 		padding: 0 8px;
@@ -204,6 +248,106 @@
 		width: 8px;
 		height: 5px;
 		background-position: -70px -543px;
+	}
+	.cat-panel {
+		position: absolute;
+		background-color: white;
+		z-index: 2;
+		width: 720px;
+		padding-top: 5px;
+		border-radius: 5px;
+		border: 1px solid rgb(150,150,150);
+		box-shadow: 0 0 10px 0px rgb(190,190,190);
+		top: 35px;
+		left: -80px;
+	}
+	.cat-panel h3 {
+		position: relative;
+		padding: 10px 0;
+		text-align: left;
+	    font-weight: normal;
+	}
+	.cat-panel h3:after {
+		content: "";
+		position: absolute;
+		width: 100%;
+		height: 1px;
+		background: rgb(220,220,220);
+		left: 0px;
+		bottom: 0px;
+	}
+	.cat-panel h3 a {
+	    width: 75px;
+    	height: 26px;
+		margin-left: 13px;
+		display: inline-block;
+		text-align: center;
+    	line-height: 26px;
+		text-decoration: none;
+		font-size: 12px;
+		color: #333;
+		background: url(http://s2.music.126.net/style/web2/img/button.png?d6b0495cd36861edbe30c32de285e077) no-repeat 0 9999px;
+		background-position: 0 -64px;
+	}
+	.cat-panel h3 a:hover {
+		text-decoration: underline;
+		color: #000;
+	}
+	.c-p-row {
+		position: relative;
+		display: flex;
+		padding: 15px 0;
+	}
+	.c-p-row:after {
+		content: "";
+		position: absolute;
+		width: 1px;
+		height: 100%;
+		background: rgb(220,220,220);
+		left: 100px;
+		top: 0px;
+	}
+	.c-p-title {
+		position: relative;
+		flex: 100px;
+		font-size: 12px;
+    	color: #333;
+    	font-weight: bold;
+	}
+	.c-p-icon {
+		margin-right: 8px;
+		width: 23px;
+	    height: 23px;
+	    display: inline-block;
+	    overflow: hidden;
+	    vertical-align: middle;
+	    background: url(http://s2.music.126.net/style/web2/img/icon.png?69dcc2eaca079ea27032fc5ff90e65a2) no-repeat 0 9999px;
+	}
+	.c-p-icon.lan{ background-position: -20px -735px; }
+	.c-p-icon.style{ background-position: -0px -60px; }
+	.c-p-icon.occasion{ background-position: 0 -88px; }
+	.c-p-icon.emotion{ background-position: 0 -117px; }
+	.c-p-icon.theme{ background-position: 0 -141px; }
+	.c-p-content {
+		flex: 620px;
+		text-align: left;
+		padding: 0 15px;
+	}
+	.c-p-c-item {
+		display: inline-block;
+	}
+	.c-p-c-item a {
+		color: #333;
+		cursor: pointer;
+	}
+	.c-p-c-item a:hover {
+		color: #000;
+		text-decoration: underline;
+	}
+	.c-p-c-item span {
+		font-size: 9px;
+		color: #ddd;
+		margin: 0 10px 0 7px;
 	}
 	/*热门、最新切换*/
 	.hot-new {
