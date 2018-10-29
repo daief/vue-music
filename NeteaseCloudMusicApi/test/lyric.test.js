70e66e5431ff40b2419e8f99ee6241f4c447497d
daief <1437931235@qq.com>
2018-10-29 15:49:22 +0800

    update api

--- NeteaseCloudMusicApi/test/lyric.test.js
+++ NeteaseCloudMusicApi/test/lyric.test.js
@@ -1,22 +1,22 @@
 const assert = require('assert')
-const crypto = require('crypto')
-const { createRequest } = require('../util/util')
+const request = require('request')
+const host = global.host || 'http://localhost:3000'
 
-describe('测试获取歌词是否正常', () => {
-  it('数据应该有 lrc 字段', done => {
-    const id = 347230
-    createRequest(
-      '/api/song/lyric?os=osx&id=' + id + '&lv=-1&kv=-1&tv=-1',
-      'GET',
-      null
-    )
-      .then(result => {
-        // console.log(JSON.parse(result).lrc)
-        assert(typeof JSON.parse(result).lrc !== 'undefined')
+describe("测试获取歌词是否正常", () => {
+  it("数据应该有 lrc 字段", done => {
+    const qs = {
+      id: 347230
+    }
+
+    request.get({url: `${host}/lyric`,qs: qs}, (err, res, body) => {
+      if (!err && res.statusCode == 200) {
+        body = JSON.parse(body)
+        assert(typeof body.lrc !== "undefined")
         done()
-      })
-      .catch(err => {
+      }
+      else{
         done(err)
-      })
+      }
+    })
   })
 })
