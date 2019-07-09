d156be2e9a86a78a0a721b2103a17384e5f4d1c9
daief <1437931235@qq.com>
2019-07-09 17:22:34 +0800

    refactor: update

---
+++ deploy-to-github.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e # exit with nonzero exit code if anything fails
+echo "Starting to update gh-pages\n"
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+
+cd dist
+git init
+git add -f .
+git commit -m "Travis build"
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/daief/vue-music.git" master:gh-pages > /dev/null
+
+echo "Done updating gh-pages\n"
+
+else
+ echo "Skipped updating gh-pages, because build is not triggered from the master branch."
+fi;
